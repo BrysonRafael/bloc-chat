93143e69de84b6a657507c9dbba22b5c61073bf2
Bryson <bryson.rafael@gmail.com>
2017-01-31 17:01:02 -0600

    blocChat.Controller Created

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,5 @@
-angular.module('blocChat', [ 'ui.router', 'firebase' ]);
+var blocChat = angular.module('blocChat', [ 'ui.router', 'firebase' ]);
+
+blocChat.controller('RoomController', ['$state', function ($state) {
+  $state.rooms = function Room($firebaseArray);
+}])
