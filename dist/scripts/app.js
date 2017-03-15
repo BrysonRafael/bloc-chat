8a8c69de5369385bf519e5aa7cdf5e2817fcda27
Bryson <bryson.rafael@gmail.com>
2017-03-14 20:13:13 -0500

    Rooms Created

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,30 +1,25 @@
 var blocChat = angular.module('blocChat', [ 'ui.router', 'firebase' ]);
 
-(function() {
-	function config($stateProvider, $locationProvider) {
-		$locationProvider
-			.html5({
-				enabled: true,
-				requireBase: false
-			});
+function config($stateProvider, $locationProvider) {
+	$locationProvider
+		.html5Mode({
+			enabled: true,
+			requireBase: false
+		});
 
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: '/templates/home.html'
-			})
-	}
+	$stateProvider
+		.state('home', {
+			url: '/',
+			controller: 'RoomController as ctrl',
+			templateUrl: '/templates/home.html'
+		})
+}
 
-	angular
-		.module('blocChat', ['ui.router'])
-		.config(config);
-})
+angular
+	.module('blocChat')
+	.config(config);
 
-blocChat.controller('RoomController', ['$state', '$firebaseArray', function ($state, $firebaseArray) {
-	var ref = firebase.database().ref();
-	var roomList = $firebaseArray(ref);
-
-
-	$state.list = roomList;
+blocChat.controller('RoomController', ['$state', 'Room', function ($state, Room) {
+	this.rooms = Room.all;
 }])
 
