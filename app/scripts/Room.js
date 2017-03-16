0441e1bf6476899e8e0ed29e224e18a8e975704d
Bryson <bryson.rafael@gmail.com>
2017-03-15 21:11:26 -0500

    Checkpoint started

--- app/scripts/Room.js
+++ app/scripts/Room.js
@@ -2,6 +2,11 @@
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
+    rooms.$add({ rooms: "room" }).then(function(ref) {
+      var id = ref.key;
+      console.log("added record with id " + id);
+      list.$indexFor(id); // returns location in the array
+    });
 
     return {
       all: rooms
