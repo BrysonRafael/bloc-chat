f93770e25d722b93b43442b60f13eea2e1e0d650
Bryson <bryson.rafael@gmail.com>
2017-04-01 19:46:19 -0500

    indent format

--- app/scripts/Room.js
+++ app/scripts/Room.js
@@ -1,24 +1,24 @@
 (function() {
-  function Room($firebaseArray) {
-    var ref = firebase.database().ref().child("rooms");
-    var rooms = $firebaseArray(ref);
-    // rooms.$add({ rooms: "room" }).then(function(ref) {
-    //   var id = ref.key;
-    //   console.log("added record with id " + id);
-    //   list.$indexFor(id); // returns location in the array
-    // });
+    function Room($firebaseArray) {
+        var ref = firebase.database().ref().child("rooms");
+        var rooms = $firebaseArray(ref);
+        // rooms.$add({ rooms: "room" }).then(function(ref) {
+        //   var id = ref.key;
+        //   console.log("added record with id " + id);
+        //   list.$indexFor(id); // returns location in the array
+        // });
 
-    var add = function add(roomName) {
-      rooms.$add(roomName);// TODO: Fill in teh codez
-    }; //- add()
+        var add = function add(roomName) {
+            rooms.$add(roomName); // TODO: Fill in teh codez
+        }; //- add()
 
-    return {
-      all: rooms,
-      add: add
-    };
-  }
+        return {
+            all: rooms,
+            add: add
+        };
+    }
 
-  angular
-    .module('blocChat')
-    .factory('Room', ['$firebaseArray', Room]);
+    angular
+        .module('blocChat')
+        .factory('Room', ['$firebaseArray', Room]);
 })();
