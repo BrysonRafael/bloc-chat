4714c2271c40594885afc81be12234ccdbd68da5
Bryson <bryson.rafael@gmail.com>
2017-04-04 20:27:19 -0500

    json

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,54 @@
 function config($stateProvider, $locationProvider) {
-	$locationProvider
-		.html5Mode({
-			enabled: true,
-			requireBase: false
-		});
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
-	$stateProvider
-		.state('home', {
-			url: '/',
-			controller: 'RoomController as ctrl',
-			templateUrl: '/templates/home.html'
-		})
+    $stateProvider
+        .state('home', {
+            url: '/',
+            controller: 'RoomController as ctrl',
+            templateUrl: '/templates/home.html'
+        })
 }
 
 var blocChat = angular
-	.module('blocChat', [ 'ui.router', 'ui.bootstrap', 'firebase' ])
-	.config(config);
+    .module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase'])
+    .config(config);
 
-blocChat.controller('RoomController', ['$state', 'Room', '$uibModal', 
-	function ($state, Room, $uibModal) {
-		this.rooms = Room.all;
+blocChat.controller('RoomController', ['$state', 'Room', 'Message', '$uibModal',
+    function($state, Room, Message, $uibModal) {
+        this.rooms = Room.all;
 
-		this.addRoom = function addRoom() {
-		    var modalInstance = $uibModal.open({
-		      templateUrl: '/templates/modal.html',
-		      controller: 'ModalController',
-		      controllerAs: 'modal'
-		    });
+        this.addRoom = function addRoom() {
+            var modalInstance = $uibModal.open({
+                templateUrl: '/templates/modal.html',
+                controller: 'ModalController',
+                controllerAs: 'modal'
+            });
 
-		    modalInstance.result.then(function(roomName) {
-		    	Room.add(roomName);
-		    });
-		}; //- addRoom()
-	}
+            modalInstance.result.then(function(roomName) {
+                Room.add(roomName);
+            });
+        }; //- addRoom()
+
+        this.setRoom = function setRoom(roomId) {
+            this.messages = Message.getMessages(roomId);
+        };
+    }
 ]);
 
+blocChat.factory('Message', ['$firebaseArray',
+    function Message($firebaseArray) {
+        var ref = firebase.database().ref().child("messages");
+
+        var getMessages = function getMessages(roomId) {
+            return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+        };
+
+        return {
+            getMessages: getMessages
+        };
+    }
+]);
