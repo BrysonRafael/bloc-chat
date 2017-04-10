a568d23729968762924d308384159d3b146d964f
Bryson <bryson.rafael@gmail.com>
2017-04-09 20:39:27 -0500

    Checkpoint Complete

--- dist/scripts/Room.js
+++ dist/scripts/Room.js
@@ -2,15 +2,9 @@
     function Room($firebaseArray) {
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
-        // rooms.$add({ rooms: "room" }).then(function(ref) {
-        //   var id = ref.key;
-        //   console.log("added record with id " + id);
-        //   list.$indexFor(id); // returns location in the array
-        // });
-
         var add = function add(roomName) {
-            rooms.$add(roomName); // TODO: Fill in teh codez
-        }; //- add()
+            rooms.$add(roomName);
+        };
 
         return {
             all: rooms,
